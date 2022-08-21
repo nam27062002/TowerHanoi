edb12050c8d575fb15c544b21eff9fa6cab28d79
nam <nam27062002@gmail.com>
2022-08-21 17:01:36 +0700

    update

--- game.js
+++ game.js
@@ -4,4 +4,24 @@ kaboom({
     scale: 1,
     debug: true,
 })
-const SPEED = 240
+const scaleTube = [500, 30]
+const NumberFloor = 6
+const distanceTube = 50
+function LoadImages() {
+    for (let i = 1; i <= NumberFloor; i++) {
+        loadSprite(i, "images/" + i + ".png")
+    }
+
+}
+LoadImages()
+scene("game", () => {
+    for (let i = 1; i <= NumberFloor; i++) {
+        console.log(i)
+        add([
+            sprite(i.toString(), { width: scaleTube[0] - distanceTube * i, height: scaleTube[1] }),
+            pos(550 + distanceTube / 2 * i, 500 - i * scaleTube[1]),
+        ])
+    }
+})
+go("game")
+
